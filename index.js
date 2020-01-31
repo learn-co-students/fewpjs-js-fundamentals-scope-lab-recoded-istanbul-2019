daf0b23d1006c9a884d2ae620493710ae9dd2cdf
merve gumuskaya <mervegumuskaya@gmail.com>
2020-01-31 10:12:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-animal = ""
+let animal = "dog"
+let yourAn = "cat"
 
 function myAnimal() {
   return animal
@@ -8,12 +9,10 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return yourAn
 }
 
 function add2(n) {
+  let two = 2
   return n + two
-
-  // Feel free to move things around!
-  two = 2
 }
